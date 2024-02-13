7b4f8fcd6aed4b9b66d1b9538064bfcd94b3d192
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-13 12:33:56 +0200

    Initial Commit

---
+++ Bucket-ios17/Bucket_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ios17App.swift
+//  Bucket-ios17
+//
+//  Created by Alin RADU on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
